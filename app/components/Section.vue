e82500781326a50601031f79aa2b2bd99b6b4552
Moozon <muzhenweizh@gmail.com>
2025-10-14 21:15:13 +0800

    style: selection style

--- app/components/Section.vue
+++ app/components/Section.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <section class="pb-6 pt-1.5">
-    <h3 class="text-white font-bold mb-4 px-2 py-0.5 rounded bg-dark-500 inline-block">
+    <h3 class="text-white font-bold mb-4 px-2 py-0.5 rounded bg-dark-500 inline-block selection:(text-black bg-white)">
       {{ title }}
     </h3>
     <div class="pl-6 lt-sm:pl-2">
