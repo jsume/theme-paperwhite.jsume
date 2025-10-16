da36ec5cae6cf0f9941ad11d53435e425bde5386
Moozon <muzhenweizh@gmail.com>
2025-10-16 15:14:51 +0800

    style(backtotop): update button size and border radius

--- app/components/BackToTop.vue
+++ app/components/BackToTop.vue
@@ -5,7 +5,7 @@ const { y } = useWindowScroll()
 <template>
   <button
     v-show="y > 100"
-    class="color-white rounded-3 bg-black flex size-6 transform-translate-x-50% transition-500 transition-all items-center bottom-5 right-50% justify-center fixed"
+    class="color-white rounded-4 bg-black flex size-8 transform-translate-x-50% transition-500 transition-all items-center bottom-5 right-50% justify-center fixed"
     @click="y = 0"
   >
     <Icon name="material-symbols:arrow-upward-rounded" />
